fb1123ad917ffa1b585c1208be3ce044c9049c78
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-05-04 21:08:51 +0530

    Configuring Webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HTMLwebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HTMLwebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
