06031068cd08d1cde62b0c2671360ab22f08267b
PabasaraPalihena <pabapalihena@gmail.com>
2022-05-28 15:20:49 +0530

    implemented the submission model,controller and routes

---
+++ server/models/Submissions.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const Submissions = new Schema({
+  submissionName: {
+    type: String,
+    require: true,
+  },
+  sType: {
+    type: String,
+  },
+  sDescription: {
+    type: String,
+  },
+  sTemplate: {
+    type: String,
+  },
+  sMarkingScheme: {
+    type: String,
+  },
+  sDeadline: {
+    type: String,
+  },
+  sVisibility: {
+    type: Boolean,
+  },
+});
+const submission = mongoose.model("Submissions", Submissions);
+module.exports = submission;
