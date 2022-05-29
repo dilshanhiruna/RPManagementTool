d66173b4eca829efa0858baceddfbd5df246643b
Tharinda Hashen Kottegoda <hashen@Hashens-MacBook-Air.local>
2022-05-30 00:38:02 +0530

    added student submission in front end

--- client/src/components/StudentGroup/Submissions.js
+++ client/src/components/StudentGroup/Submissions.js
@@ -9,11 +9,27 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import SubmissionItem from "./SubmissionItem";
+import { LinearProgress } from "@mui/material";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from "@mui/material";
+import FileBase64 from "react-file-base64";
+
 const API = process.env.REACT_APP_API;
 import axios from "axios";
 export default function Submissions({ studentGroup }) {
   const [Group, setGroup] = useState(studentGroup);
   const [submissionDetails, setSubmissionDetails] = useState([]);
+  const [pageIsLoadig, setPageIsLoading] = useState(true);
+  const [openConfirmModal, setOpenConfirmModal] = useState(false);
+  const [selectedSubmissionDetail, setSelectedSubmissionDetail] =
+    useState(false);
+  const [studentSubmission, setStudentSubmission] = useState({});
+
   // Submission Types
   // TopicAssesmentForm
   // ProjectProposal & Presentation (ppt)
@@ -49,7 +65,13 @@ export default function Submissions({ studentGroup }) {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small" variant="contained">
+          <Button
+            size="small"
+            variant="contained"
+            onClick={() => {
+              handleModalOpen(submissionDetail);
+            }}
+          >
             Submit
           </Button>
           <Button size="small" variant="text">
@@ -68,6 +90,34 @@ export default function Submissions({ studentGroup }) {
       const result = await axios.get(`${API}/AssignmentSubmissions/active`);
       console.log(result.data.data);
       setSubmissionDetails(result.data.data);
+      setPageIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleModalClose = () => {
+    setOpenConfirmModal(false);
+  };
+
+  const handleModalOpen = (submissionDetail) => {
+    setOpenConfirmModal(true);
+    setSelectedSubmissionDetail(submissionDetail);
+  };
+
+  const onFileUpload = (file) => {
+    setStudentSubmission({ file });
+    console.log(file);
+  };
+
+  const onSubmitConfirm = async () => {
+    try {
+      const result = await axios.post(`${API}/`, studentSubmission);
+      if (result.data.success) {
+        alert("submission success");
+      } else {
+        alert("error");
+      }
     } catch (err) {
       console.error(err);
     }
@@ -76,54 +126,97 @@ export default function Submissions({ studentGroup }) {
     getSubmissionDetails();
   }, []);
   return (
-    <div>
-      <div style={{ textAlign: "left" }}>
-        <h4>Group ID: {studentGroup.groupID} </h4>
-        <h4>Topic: {studentGroup.researchTopic}</h4>
-        <h4>
-          Supervisor :
-          {studentGroup.supervisor?.name
-            ? " " +
-              studentGroup.supervisor?.uid +
-              " - " +
-              studentGroup.supervisor?.name
-            : "Not Assigned"}{" "}
-          (
-          {studentGroup.supervisorStatus !== "none"
-            ? studentGroup.supervisorStatus
-            : ""}
-          )
-        </h4>
-        <h4>
-          Co-Supervisor :
-          {studentGroup.cosupervisor?.name
-            ? " " +
-              studentGroup.cosupervisor?.uid +
-              " - " +
-              studentGroup.cosupervisor?.name
-            : "Not Assigned"}{" "}
-          (
-          {studentGroup.cosupervisorStatus !== "none"
-            ? studentGroup.cosupervisorStatus
-            : ""}
-          )
-        </h4>
+    <>
+      {" "}
+      <div>
+        <div style={{ textAlign: "left" }}>
+          <h4>Group ID: {studentGroup.groupID} </h4>
+          <h4>Topic: {studentGroup.researchTopic}</h4>
+          <h4>
+            Supervisor :
+            {studentGroup.supervisor?.name
+              ? " " +
+                studentGroup.supervisor?.uid +
+                " - " +
+                studentGroup.supervisor?.name
+              : "Not Assigned"}{" "}
+            (
+            {studentGroup.supervisorStatus !== "none"
+              ? studentGroup.supervisorStatus
+              : ""}
+            )
+          </h4>
+          <h4>
+            Co-Supervisor :
+            {studentGroup.cosupervisor?.name
+              ? " " +
+                studentGroup.cosupervisor?.uid +
+                " - " +
+                studentGroup.cosupervisor?.name
+              : "Not Assigned"}{" "}
+            (
+            {studentGroup.cosupervisorStatus !== "none"
+              ? studentGroup.cosupervisorStatus
+              : ""}
+            )
+          </h4>
 
-        <Divider />
+          <Divider />
 
-        <h1>Group Submissions</h1>
-        <div style={{ maxHeight: 600, overflow: "auto" }}>
-          {submissionDetails.map((submissionDetail) => {
-            return (
-              <>
-                <BasicCard submissionDetail={submissionDetail} />
-                <br></br>
-              </>
-            );
-          })}
+          <h1>Group Submissions</h1>
+          <div className="student__dashboard">
+            {pageIsLoadig ? <LinearProgress color="inherit" /> : ""}
+          </div>
+          <div style={{ maxHeight: 600, overflow: "auto" }}>
+            {submissionDetails.map((submissionDetail) => {
+              return (
+                <>
+                  <BasicCard submissionDetail={submissionDetail} />
+
+                  <br></br>
+                </>
+              );
+            })}
+            <Dialog
+              open={openConfirmModal}
+              // onClose={handleCloseRemoveMember}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {selectedSubmissionDetail.submissionName}
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  {selectedSubmissionDetail.sDescription}
+                </DialogContentText>
+              </DialogContent>
+              <DialogContent className="centerItems">
+                <FileBase64 multiple={false} onDone={onFileUpload.bind()} />
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  onClick={() => {
+                    handleModalClose();
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  // onClick={() => {
+                  //   acceptOrReject();
+                  // }}
+                  autoFocus
+                  color="error"
+                >
+                  Submit
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
         </div>
+        <Box sx={{ maxWidth: 400 }}></Box>
       </div>
-      <Box sx={{ maxWidth: 400 }}></Box>
-    </div>
+    </>
   );
 }
