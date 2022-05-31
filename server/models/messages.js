af8b4bb5690ec41f3c1e0205b0c5a0d22ee18f30
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-05-31 18:20:17 +0530

    Combined Chat Server

---
+++ server/models/messages.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const msgSchema = new mongoose.Schema({
+  room: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+});
+
+const Msg = mongoose.model("msg", msgSchema);
+module.exports = Msg;
