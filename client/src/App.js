7fa7c2a98749fb4166f3f29e2b89c316a56c6a2e
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-05-31 18:55:48 +0530

    Created Signin/Signup forms

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Switch, BrowserRouter as Router } from "react-router-dom";
 import Student from "./routes/Student";
 import Staff from "./routes/Staff";
 import Admin from "./routes/Admin";
+import Signup from "./components/view/Signup";
+import Signin from "./components/view/Signin";
 // import Supervisor from "./routes/Supervisor";
 
 function App() {
