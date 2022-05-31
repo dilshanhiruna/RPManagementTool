8a22969e174dcca19614d69c06532ea375cb9729
Hashen Kottegoda <thkottegoda@gmail.com>
2022-05-31 19:46:07 +0530

    added two new backend endpoints getSubmissionOfSupervisor and getSubmissionsOfPanelMember

--- server/routes/StudentSubmission.js
+++ server/routes/StudentSubmission.js
@@ -5,8 +5,12 @@ const {
   createStudentSubmission,
   getSpecificStudentSubmission,
   deleteStudentSubmission,
+  getSubmissionOfSupervisor,
+  getSubmissionsOfPanelMember,
 } = require("../controllers/StudentSubmission");
 
+router.get("staff/:id", getSubmissionOfSupervisor);
+router.get("panel/:id", getSubmissionsOfPanelMember);
 router.post("/", createStudentSubmission);
 router.delete("/:id", deleteStudentSubmission);
 router.post("/getSpecific", getSpecificStudentSubmission);
