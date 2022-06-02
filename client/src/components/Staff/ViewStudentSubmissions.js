a10f9f0ff83a5d5395b87aa4db924d4bc1d61a5c
Hashen Kottegoda <thkottegoda@gmail.com>
2022-06-02 19:56:19 +0530

    updated code quality and added coments to ViewStudentSubmissions

--- client/src/components/Staff/ViewStudentSubmissions.js
+++ client/src/components/Staff/ViewStudentSubmissions.js
@@ -24,82 +24,37 @@ import { LinearProgress } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { Snackbar } from "@mui/material";
 import MuiAlert from "@mui/material/Alert";
+import { columns, createObjResponse } from "./utils/viewStudentSubmissionsUtil";
 
+//alert for snackbar
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-const columns = [
-  { id: "groupID", label: "Group ID", minWidth: 100, align: "center" },
-  {
-    id: "submissionName",
-    label: "Submission Name",
-    minWidth: 100,
-    align: "center",
-  },
-  {
-    id: "deadline",
-    label: "Deadline",
-    minWidth: 100,
-    align: "center",
-  },
-  {
-    id: "submittedOn",
-    label: "Submitted On",
-    minWidth: 100,
-    align: "center",
-  },
-  {
-    id: "marking",
-    label: "Marking Scheme",
-    minWidth: 100,
-    align: "center",
-  },
-  {
-    id: "submission",
-    label: "Submission",
-    minWidth: 100,
-    align: "center",
-  },
-  {
-    id: "marks",
-    label: "Provided Marks",
-    minWidth: 100,
-    align: "center",
-  },
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
 const API = process.env.REACT_APP_API;
 
 export default function ViewStudentSubmissions({ user }) {
+  //hooks for table pagination
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [rows, setRows] = useState([]);
   const [pageIsLoadig, setPageIsLoading] = useState(true);
-
   //modal varivales
   const [openAddModal, setopenAddModal] = useState(false);
   const [openEditModal, setopenEditModal] = useState(false);
-
+  //validation hooks
   const [valError, setValError] = useState(false);
   const [helperText, setHelperText] = useState("");
+  //hook to set new marks
   const [marks, setMarks] = useState(50);
-
   //submission details of selected row (cuurent context)
   const [currentSubmissionId, setcurrentSubmissionId] = useState();
   const [currentgroupId, setcurrentgroupId] = useState();
   const [currentsubmissionName, setcurrentsubmissionName] = useState();
   const [currentMarks, setcurrentMarks] = useState();
-
   //handle search vars
   const [searchByGroupId, setsearchByGroupId] = useState(null);
   const [searchBySubmissionName, setsearchBySubmissionName] = useState(null);
-
   //for confirmation snackbar
   const [openAlert, setopenAlert] = useState(false);
   const [showErrorr, setshowErrorr] = useState(false);
@@ -108,13 +63,12 @@ export default function ViewStudentSubmissions({ user }) {
   useEffect(() => {
     getStudentSubmissions();
   }, []);
+
   //function to get student submissions of supervior and cosupervisor
   const getStudentSubmissions = async () => {
     try {
       let objArray = [];
-
       await axios
-
         .get(`${API}/studentSubmission/staff/${user._id}`)
         .then((res) => {
           if (res.data.data.length == 0) {
@@ -127,45 +81,12 @@ export default function ViewStudentSubmissions({ user }) {
             });
           }
         });
-
       setPageIsLoading(false);
       setRows(objArray);
     } catch (err) {
       console.log(err);
     }
   };
-
-  //function to create obj from server response
-  const createObjResponse = (res, data) => {
-    if (!data.submissionDetailsId || !data.studentGroupId) {
-      return null;
-    }
-    let obj = {
-      _id: data._id,
-      groupID: data.studentGroupId.groupID,
-      submissionName: data.submissionDetailsId.submissionName,
-      deadline: data.submissionDetailsId.sDeadline,
-      submittedOn: formatDate(data.submittedOn),
-      marking: data.submissionDetailsId.sMarkingScheme,
-      // marking: data.studentGroupId.groupID,
-      submission: data.file,
-      marks: data.obtainedMarks,
-    };
-    return obj;
-  };
-
-  //function to format js date
-  function formatDate(date) {
-    var d = new Date(date),
-      month = "" + (d.getMonth() + 1),
-      day = "" + d.getDate(),
-      year = d.getFullYear();
-
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    return [year, month, day].join("-");
-  }
 
   //function to handle marks form input
   const handleMarkChange = async (e) => {
@@ -189,6 +110,7 @@ export default function ViewStudentSubmissions({ user }) {
       console.log(marks + "..." + 3);
     }
   };
+
   //function to provide marks for student submissions
   const addMarks = async () => {
     setshowErrorr(false);
