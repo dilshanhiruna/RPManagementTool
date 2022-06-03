2e28bc52df5401527571b41bc66a195b532367da
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-03 09:58:20 +0530

    Fixed minor issues

--- client/src/routes/LoginRouter.js
+++ client/src/routes/LoginRouter.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 // import Header from '../components/view/Header';
-// import Footer from '../components/view/Footer';
+import Footer from "../components/view/Footer";
 // import AdminViewMovies from '../components/MovAdmin/AdminViewMovies';
 // import MovAdminHeader from '../components/view/MovAdminHeader';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Login from '../components/Login/index';
-import Signup from '../components/Signup/index';
-import Logout from '../components/Logout/index';
+import { Route, Switch, Redirect } from "react-router-dom";
+import Login from "../components/Login/index";
+import Signup from "../components/Signup/index";
+import Logout from "../components/Logout/index";
 
 export default function LoginRouter() {
   return (
-    <div className='movadmin__view'>
+    <div className="movadmin__view">
       {/* <MovAdminHeader title={'Movies'} user={'movadmin'} /> */}
       <Switch>
-        <Route exact path='/login'>
+        <Route exact path="/login">
           <Login />
         </Route>
-        <Route exact path='/signup'>
+        <Route exact path="/signup">
           <Signup />
         </Route>
-        <Route exact path='/logout'>
+        <Route exact path="/logout">
           <Logout />
         </Route>
-        <Redirect to='/login' />
+        <Redirect to="/login" />
       </Switch>
       <Footer />
     </div>
